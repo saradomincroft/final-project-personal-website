109a8ec0c185f0d0a58ae33eb3f5811eae597af4
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2023-11-19 15:35:27 +1100

    I have added a function to index.js so that when the only the selected section is showing. The about section will load on default open, but the current section will stay on refresh. I have also made it so that the styling applied to the active section is saved in local storage with js so that when it is refreshed, the tab link will stay stylised. I have also added a jquery file in case I use jquery but I might not and might delete later

--- js/index.js
+++ js/index.js
@@ -1,36 +1,57 @@
 function openTab(event, tabName) {
     var i, section;
 
-    // Hide sections
-    section = document.getElementsByClassName("section");
+    // Hide main sections (About, Portfolio, Resume, Skills, Contact)
+    section = document.getElementsByClassName('section');
     for (i = 0; i < section.length; i++) {
-        section[i].style.display = "none";
+        section[i].style.display = 'none';
     }
 
     // Display selected section
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.display = 'block';
+
+    // Remove active class from all tab links
+    var tabLinks = document.querySelectorAll('.tablinks');
+    tabLinks.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+
+    // Add active class to active tab
+    event.currentTarget.classList.add('active');
 
     // Store selected tab in local storage (for refreshing and staying on same tab)
+    // Setter and Getter (Setter)
     localStorage.setItem('activeTab', tabName);
 }
 
-// Function to display the 'About' section on page load
+// Function to display the about section as default on page load but stay on selected page on refreshe
 window.onload = function() {
     // Get active tab from local storage
+    // Setter and Getter (Getter)
     var activeTab = localStorage.getItem('activeTab');
 
-    // On refresh (from stored) display active tab, otherwise default to 'about' tab
+    // On refresh (from stored) display active tab, otherwise default to about tab
     if (activeTab) {
         // Hide sections
         var sectionsToHide = document.getElementsByClassName('section');
         for (var i = 0; i < sectionsToHide.length; i++) {
             sectionsToHide[i].style.display = 'none';
         }
-
         // Show stored active tab
-        document.getElementById(activeTab).style.display = 'block';
+        var activeTabElement = document.getElementById(activeTab);
+        if (activeTabElement) {
+            activeTabElement.style.display = 'block';
+
+            // Find corresponding tab based on section ID and add active class styling
+            var currentTab = document.querySelector('.tablinks[onclick*="' + activeTab + '"]');
+            if (currentTab) {
+                currentTab.classList.add('active');
+            }
+        }   
+        
     } else {
         // Display the 'about' section by default
-        document.getElementById('about').style.display = 'block';
+        document.getElementById("about").style.display = 'block';
     }
 };
+
